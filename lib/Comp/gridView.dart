a88d2a9846c2b2831498f57055328fa6185e5a2b
Sketchzy <59944324+indunil97@users.noreply.github.com>
2020-06-08 23:03:42 +0530

    UI changed to new one

--- lib/Comp/gridView.dart
+++ lib/Comp/gridView.dart
@@ -33,12 +33,14 @@ class _itemsState extends State<items> {
       "price": "\$40.99"
     }
   ];
+
+  //building list view
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
         itemCount: products.length,
         gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 1,crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
           return SingleProduct(
               productName: products[index]['name'],
@@ -60,12 +62,29 @@ class SingleProduct extends StatelessWidget {
       this.productImage,
       this.productOld,
       this.productPrice});
+
+      //one card
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
+      padding: EdgeInsets.fromLTRB(6, 6, 6, 6),
+      decoration:BoxDecoration(
+        borderRadius:BorderRadius.circular(15.0),
+        boxShadow:[
+          BoxShadow(color: Colors.grey.withOpacity(0.2),
+          spreadRadius:4.0,
+          blurRadius:96.0)
+        ]
+      ),
         child: Hero(
             tag: Text("hero1"), //can coused a error
             child: Material(
+              elevation: 1.0,
+              shape: RoundedRectangleBorder(
+                borderRadius:BorderRadius.circular(16),
+                
+              ),
+            
               child: InkWell(
                 onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context){return ProductDetails(
                   ProductDetails_Name: productName,
@@ -73,29 +92,45 @@ class SingleProduct extends StatelessWidget {
                   ProductDetails_Old: productOld,
                   ProductDetails_Price: productPrice,
                 );}));},
-                child: GridTile(
-                  footer: Container(
-                      color: Colors.white70,
-                      child: ListTile(
-                        leading: Container(
-                            
-                            child: Text(
-                              productName,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                            )),
-                        title: Text(
-                          '\$$productPrice',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          '\$$productOld',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.lineThrough),
-                        ),
-                      )),
-                  child: Image.asset(productImage, fit: BoxFit.cover),
+                child: Container(
+                  
+                  decoration: BoxDecoration(
+                    
+                    borderRadius: BorderRadius.circular(11.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 3.0,
+                          blurRadius: 5.0)
+                    ],
+                    color: Colors.white),
+                  child: GridTile(
+                    footer: Container(
+                      
+                      
+                      
+                        color: Colors.transparent,
+                        child: ListTile(
+                          leading: Container(
+                              
+                              child: Text(
+                                productName,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              )),
+                          title: Text(
+                            '\$$productPrice',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            '\$$productOld',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.lineThrough),
+                          ),
+                        )),
+                    child: Icon(Icons.add_shopping_cart),
+                  ),
                 ),
               ),
             )));
