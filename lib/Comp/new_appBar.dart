5cc541e8168fcbac3fea54adaf6aebdb1bcbb116
Sketchzy <59944324+indunil97@users.noreply.github.com>
2020-06-09 21:50:14 +0530

    rearranged

---
+++ lib/Comp/new_appBar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+
+class CustumAppBar extends StatefulWidget {
+  @override
+  _CustumAppBarState createState() => _CustumAppBarState();
+}
+
+class _CustumAppBarState extends State<CustumAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+
+    );
+  }
+}
