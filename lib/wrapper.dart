14eedd44616d5cf6f2e2660b1cd49ad6f685d10e
Sketchzy <59944324+indunil97@users.noreply.github.com>
2020-06-14 01:59:01 +0530

    Update wrapper.dart

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -4,6 +4,7 @@ import 'package:Shopping/Comp/pages/homepage.dart';
 import 'package:Shopping/authenticate.dart';
 import 'package:Shopping/models/user.dart';
 import 'package:Shopping/services/auth.dart';
+import 'package:Shopping/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,10 +15,12 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    if (user == null){
-      return Authenticate();
-    }else{
+    print("user");
+    
+    if (user != null){
       return FirstPage();
+    }else{
+      return Authenticate();
     }
   
   }
