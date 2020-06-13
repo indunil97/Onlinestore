22b891adaeff4500aea184b5cb94988aebfb77c2
Sketchzy <59944324+indunil97@users.noreply.github.com>
2020-06-14 01:59:24 +0530

    Create constant.dart

---
+++ lib/shared/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const textFieldDecoration = InputDecoration(
+    
+    labelStyle: TextStyle(color: Colors.grey), //font
+    focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.greenAccent)));
